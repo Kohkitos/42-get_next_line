5f722eb6350423b60cd40e520fde5637ea8e3d65
Kohkitos <sanzyagain@gmail.com>
2024-01-24 12:28:10 +0100

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:22:19 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/19 18:24:19 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/19 18:26:24 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 
