caf3ac1fe771b1c9a35dee7878977992c9b18ac4
Kohkitos <sanzyagain@gmail.com>
2024-01-24 13:11:47 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,47 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:23:43 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/19 18:24:09 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/24 13:10:47 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int	check_n(char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '/n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+void	read_fd(int fd, char *buffer)
+{
+	int	i;
+
+	if (!buffer)
+		buffer = 0;
+	while (read(fd, buffer, BUFFER_SIZE) > 0)
+	{
+		i = check_n(buffer);
+		if (i)
+			break ;
+		i = -1;
+	}
+}
+
 char	*get_next_line(int fd)
 {
-	return ;
+	static char	*buffer;
+
+	if (fd < 0)
+		return (NULL);
+	read_fd(fd, buffer);
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:22:19 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/19 18:26:24 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/24 13:04:52 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
