2d2c85b67e6d8fc02feb8268a2df30bbd04c51c5
Kohkitos <sanzyagain@gmail.com>
2024-01-24 13:17:11 +0100

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fsanz-go <fsanz-go@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:22:19 by fsanz-go          #+#    #+#             */
-/*   Updated: 2024/01/24 13:04:52 by fsanz-go         ###   ########.fr       */
+/*   Updated: 2024/01/24 13:16:33 by fsanz-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
